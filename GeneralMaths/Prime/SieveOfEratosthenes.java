c76bdb73ad120dcd05e9f0349828ab816d61dc25
Sagar <sagar@Sagars-MacBook-Air.local>
2023-03-28 23:02:33 +0530

    Sieve of Eratosthenes

---
+++ GeneralMaths/Prime/SieveOfEratosthenes.java
@@ -0,0 +1,26 @@
+package GeneralMaths.Prime;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        int n = 400;
+
+        boolean prime[] = new boolean[n + 1];
+        for(int i = 1; i <= n; i++) {
+            prime[i] = true;
+        }
+
+        for(int p = 2; p * p <= n; p++) {
+            for(int i = p * p; i <= n; i += p) {
+                prime[i] = false;
+            }
+        }
+
+        for(int i = 2; i <= n; i++) {
+            if(prime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+    }
+}
